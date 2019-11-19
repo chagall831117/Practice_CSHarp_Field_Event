3f09178ea206e16010b33e55ed72a6c9cc3326f4
chagall831117 <chagall831117@users.noreply.github.com>
2019-11-19 11:03:11 +0800

    增加事件與欄位

---
+++ UnityProject/Assets/chicken.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine; //Unity API
+
+public class chicken : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Range(0,100)]
+    [Header("移動速度")]
+    public float Speed = 10; //移動速度
+    [Range(0, 2000)]
+    [Header("跳躍高度")]
+    public int Jump = 200; //跳躍高度
+    [Header("對話內容")]
+    public string Talk = "咕咕咕~~~";//字串
+    [Header("是否取得雞蛋")]
+    public bool egg = false;//是否取得雞蛋
+    void Start()
+    {
+        print("遊戲開始~~~");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Talk);
+    }
+}
